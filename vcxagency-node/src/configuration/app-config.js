6480bcd842396a31db5f22db6f77112361b7c71e
Miroslav Kovar <miroslavkovar@protonmail.com>
2021-02-08 10:14:38 +0100

    Allow empty
    Signed-off-by: Miroslav Kovar <miroslavkovar@protonmail.com>

--- vcxagency-node/src/configuration/app-config.js
+++ vcxagency-node/src/configuration/app-config.js
@@ -70,8 +70,8 @@ const configValidation = Joi.object().keys({
   PG_WALLET_MAX_CONNECTIONS: Joi.number().integer().min(50).max(999),
   PG_WALLET_MIN_IDLE_COUNT: Joi.number().integer().min(0).max(0), // max 1 enforced on plugin level for MultiWalletSingleTableSharedPool strategy
   PG_WALLET_CONNECTION_TIMEOUT_MINS: Joi.number().integer().min(1).max(100),
-  AWS_BUCKET_NAME: Joi.string(),
-  AWS_DOMAIN_NAME: Joi.string()
+  AWS_BUCKET_NAME: Joi.string().allow(''),
+  AWS_DOMAIN_NAME: Joi.string().allow('')
 })
 
 async function validateAppConfig (appConfig) {
